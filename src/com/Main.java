93975337f45de8a85bde1a0599fdea197805b0be
Kristo Godari <kristo.godari@gmail.com>
2016-02-08 22:06:57 +0200

    Adding comments to Main.class.

--- src/com/Main.java
+++ src/com/Main.java
@@ -8,17 +8,21 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		
+		// decalring input and output file path
 		String inputFilePath = "C:\\Users\\Kristo\\workspace\\HashCode\\files\\logo.in";
 		String outPutFilePath = "C:\\Users\\Kristo\\workspace\\HashCode\\files\\logo.out";
 		
+		// ReadFromFile and  WriteToFile object initialization
 		ReadFromFile  readFromFileObject = new ReadFromFile(inputFilePath);
 		WriteToFile   writeToFileObject  = new WriteToFile(outPutFilePath);
 		
+		// Reading from file and outputing the result
 		String[] fileContent = readFromFileObject.readFile();		
 		for(int i=0; i< fileContent.length; i++){
 			System.out.println(fileContent[i]);
 		}
 		
+		// Writing the above output to a file
 		writeToFileObject.writeFile(fileContent);
 	}
 
