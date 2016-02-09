c712909f9737f4f8ec49c176391a340fe9da64d5
Kristo Godari <kristo.godari@gmail.com>
2016-02-10 00:46:39 +0200

    Empty commit

--- src/com/Main.java
+++ src/com/Main.java
@@ -37,6 +37,7 @@ public class Main {
 			System.out.println(fileCharContent[i]);
 		}
 		System.out.println("\n\n");
+		
 
 		// Test that readColumnAsString function works
 		int columnNumber = 11;
