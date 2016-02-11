018b412a91e42f7e681f6dce3273985c9445acf2
best4him <c_ristinescu@yahoo.com>
2016-02-11 23:24:25 +0200

    sdf

--- src/com/Main.java
+++ src/com/Main.java
@@ -2,12 +2,14 @@ package com;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import models.Command;
 import models.InputDataObject;
 
 import models.Command.CommandType;
-
+import models.Drone;
 import models.Location;
 import models.Order;
 import models.Product;
@@ -36,8 +38,11 @@ public class Main {
 		InputDataObject redundancy = new InputDataObject(pathToIntFolder, "redundancy");
 		ArrayList<Product> productsToBeDelivery = new ArrayList<Product>();
 		for (Order order : motherOfAllWarehouses.getOrders()) {
-			productsToBeDelivery.addAll(order.getProductsToBeDelibered());
+			productsToBeDelivery.addAll(getOrderProcessedProducts(order));
 		}
+		
+		ArrayList<Drone> drone = Drone.createDefault(motherOfAllWarehouses.getNrDrones());
+		
 		for(step=0; step < motherOfAllWarehouses.getDeadLineOfSimulation(); step++) {
 
 		}
@@ -48,6 +53,27 @@ public class Main {
 		double d = Math.sqrt(Math.pow(Math.abs(a.getCol()-b.getCol()), 2) + Math.pow(Math.abs(a.getRow()-b.getRow()), 2) );
 		
 		return (int)Math.ceil(d);
+	}
+	
+	public static ArrayList<Product> getOrderProcessedProducts (Order order) {
+		
+		ArrayList<Product> products = order.getProductsToBeDelibered();
+		 Set<Integer> set = new HashSet<Integer>();
+		 for (Product product : products) {
+			 set.add(product.getType());
+		 }
+		 
+		 ArrayList<Product> rp= new ArrayList<Product>();
+		 for (Integer pr : set) {
+			Product p =  new Product(pr.intValue());
+			p.setQuantity(Utils.getNumberOfProduct(products, pr.intValue()));
+			p.setLocationToBeDeliver(order.getLocationToBeDelivered());
+			rp.add(p);
+		 }
+
+	
+		 
+		 return rp;
 	}
 	
 
